2876922809378250e84ec18d6277f4a03ab96c5b
Deivy Lujan <developdeivy@gmail.com>
2026-02-13 11:29:39 -0500

    :art: structure created

---
+++ src/main/java/com/example/project_one_colaboration_team/ProjectOneColaborationTeamApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_one_colaboration_team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectOneColaborationTeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectOneColaborationTeamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/project_one_colaboration_team/ProjectOneColaborationTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project_one_colaboration_team;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectOneColaborationTeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
